2fa7d7185cba2fa93b016ec8570e758edcbf8aeb
talal-web <maliktalal910@gmail.com>
2026-02-16 14:47:04 +0500

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {/* Wrap the app content in a div with id="root" for react-modal */}
         <div id="root">{children}</div>
         <Footer />
       </body>
